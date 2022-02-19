3ecf9692c18b423dfd10718cfbfc5f43fc430577
valeriesutera <valeriesutera@gmail.com>
2022-02-18 23:28:16 -0500

    use effect

--- src/App.js
+++ src/App.js
@@ -1,55 +1,33 @@
 import './App.css';
-import restaurant from './restaurant.jpeg';
-
-//components are functions that return a little bit of UI and we can compose fucntions together to create a larger app
-function Header(props) {
-  return (
-  <header>
-    <h1>{props.name}'s Kitchen</h1>
-  </header>
-  );
-}
-
-//need to use Javascript camelcase
-//omstead of importing photo you could get an image from github
-function Main(props) {
-  return (
-    <section>
-      <p> We serve the {props.adjective} food </p>
-      <img src={restaurant} height={200} alt="burger"/>
-      <ul style = {{textAlign: "left"}}> 
-        {props.dishes.map((dish)=> 
-        <li key={dish.id}>{dish.title}</li>)}
-      </ul>
-
-    </section>
-  );
-}
-
-function Footer(props) {
-  return (
-    <footer>
-      <p> Copyright {props.year} </p>
-    </footer>
-  );
-}
-
-const dishes = ["Mac and Cheese","Bacon Cheese Burger", "Bacon Cheese Fries"];
-
-//transofrmation function to have alist of objects to avoid key error
-//building object before passing it to component as a property
-const dishObjects = dishes.map((dish,i) => ({id:i , title:dish}));
-//console.log(dishObjects)
+import React, {useState, useEffect} from "react";
 
 //we create a component by creating a function that returns jsx (this ui element we can add to the DOM)
 function App() {
-  return (
-    <div className="App">
-      <Header name="Val"/>
-      <Main adjective="amazing" dishes={dishObjects}/>
-      <Footer year={new Date().getFullYear()}/>
-    </div>
-  );
+  const [emo, setEmo] = useState("happy");
+  const[secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log('we are feeling', emo);
+  },[emo]);
+
+  useEffect(() => {
+    console.log('we are feeling', secondary);
+  },[secondary]);
+
+  return(
+    <>
+      <h1> current emotion is {emo} and {secondary} </h1>
+      <button onClick={() => setEmo("frustrated")}> Frustrate
+      </button>
+      <button onClick={() => setEmo("enthusiastic")}> Enthuse
+      </button>
+      <button onClick={() => setEmo("happy")}> Happy
+      </button>
+      <button onClick={() => setSecondary("Crabby")}> Crabby
+      </button>
+    </>
+  )
+
 }
 
 export default App; //this means it has to be imported
--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'
 
+//you can only run one component unless you wrap componenets in divs
+//A way to get around this is to use React.Fragment or just <></>
+
 //first argument it takes in is the element we want to take in 
 //second argument is where you want to put it in the index.html
+
+// const [,,third] = ["boots","tent","lamp"];
+// console.log(third);
+
 ReactDOM.render(
   //React.createElement("h1",{style:{color:"blue"}}, "Heyy everyone"), //using javascript to create html elements; 2nd element is a property
   // <ul>
